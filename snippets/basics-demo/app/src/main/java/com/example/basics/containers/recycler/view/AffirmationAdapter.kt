74b3dd95c2f657fa2ddd551b2db8f58d4f15078d
hofls <hofls@ya.ru>
2021-12-06 11:58:07 +0500

    RecyclerView

--- snippets/basics-demo/app/src/main/java/com/example/basics/containers/recycler/view/AffirmationAdapter.kt
+++ snippets/basics-demo/app/src/main/java/com/example/basics/containers/recycler/view/AffirmationAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.basics.R
@@ -13,8 +14,9 @@ class AffirmationAdapter(
     private val dataset: List<AffirmationRepository.Affirmation>
 ) : RecyclerView.Adapter<AffirmationAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     /** .xml layout */
@@ -28,7 +30,9 @@ class AffirmationAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = item.message
+        holder.imageView.setImageResource(item.imageResourceId)
     }
+
 
     override fun getItemCount() = dataset.size
 
--- snippets/basics-demo/app/src/main/java/com/example/basics/containers/recycler/view/AffirmationRepository.kt
+++ snippets/basics-demo/app/src/main/java/com/example/basics/containers/recycler/view/AffirmationRepository.kt
@@ -1,14 +1,20 @@
 package com.example.basics.containers.recycler.view
 
+import androidx.annotation.DrawableRes
+import com.example.basics.R
+
 class AffirmationRepository {
 
-    data class Affirmation(val message: String) {}
+    data class Affirmation(
+        val message: String,
+        @DrawableRes val imageResourceId: Int
+    )
 
     fun findAffirmations(): List<Affirmation> {
         return listOf(
-            Affirmation("Text A"),
-            Affirmation("Text B"),
-            Affirmation("Text C")
+            Affirmation("Text A", R.drawable.image7),
+            Affirmation("Text B", R.drawable.image7),
+            Affirmation("Text C", R.drawable.image7)
         )
     }
 
