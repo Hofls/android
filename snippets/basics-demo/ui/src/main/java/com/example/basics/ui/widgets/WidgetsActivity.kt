bdf89dffb63a0b7aa4749008805e73222dedade3
hofls <hofls@ya.ru>
2021-12-13 15:20:40 +0500

    Dropdown/Popup menu

--- snippets/basics-demo/ui/src/main/java/com/example/basics/ui/widgets/WidgetsActivity.kt
+++ snippets/basics-demo/ui/src/main/java/com/example/basics/ui/widgets/WidgetsActivity.kt
@@ -1,10 +1,12 @@
 package com.example.basics.ui.widgets
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.example.basics.R
+import com.example.basics.ui.containers.ContainersActivity
 
 class WidgetsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +16,9 @@ class WidgetsActivity : AppCompatActivity() {
 
     fun showToast(view: View) {
         Toast.makeText(this, "Hello toast!", Toast.LENGTH_SHORT).show()
+    }
+
+    fun toMenu(view: View) {
+        startActivity(Intent(this, MenuActivity::class.java))
     }
 }
